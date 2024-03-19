1f1f46b65d4f95b1c371a9d96c7f989bbd4e59e3
Altripayo <b.r.van.der.woude@student.rug.nl>
2024-03-19 19:26:18 +0100

    Added Dataset handling and corresponding unit tests.

--- src/organoids.py
+++ src/organoids.py
@@ -1,16 +1,30 @@
-# import torch
+import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.io import read_image
+
+import pandas as pd
 
 
 class Organoids(Dataset):
-    def __init__(self, file="organoids.csv", path="", enable_preprocessing=True):
-        # get data csv
-        pass
+    def __init__(self, file, path="./", transform=None):
+        self.path = path + "organoid_data/"
+        self.df = pd.read_csv(path + file, header=None, names=["path"])
+        if transform is None:
+            self.transform = transforms.Compose([
+                transforms.Resize(320, antialias=True),
+                transforms.ConvertImageDtype(torch.float32),
+                transforms.Normalize(mean=[0.5], std=[0.5]),
+            ])
+        else:
+            self.transform = transform
 
     def __len__(self):
-        # number of data samples
-        pass
+        return len(self.df)
 
     def __getitem__(self, idx):
-        # preprocess and return sample
-        pass
+        img_path = self.path + self.df.iloc[idx, 0]
+        image = read_image(img_path)
+        image = self.transform(image)
+
+        return image
--- tests/unit.py
+++ tests/unit.py
@@ -1,2 +1,29 @@
-def test_boilerplate():
-    assert True
+import torch
+
+import src.organoids as organoids
+
+
+def test_organoids_dataset():
+    dataset = organoids.Organoids(file="utils/downstream_train.csv")
+    sample = dataset[0]
+
+    assert len(dataset) == 40636
+    assert sample.shape == (1, 320, 320)
+    assert torch.max(sample) <= 1.0
+    assert torch.min(sample) >= -1.0
+
+    dataset = organoids.Organoids(file="utils/pretext_train.csv")
+    sample = dataset[0]
+
+    assert len(dataset) == 40631
+    assert sample.shape == (1, 320, 320)
+    assert torch.max(sample) <= 1.0
+    assert torch.min(sample) >= -1.0
+
+    dataset = organoids.Organoids(file="utils/test.csv")
+    sample = dataset[0]
+
+    assert len(dataset) == 20322
+    assert sample.shape == (1, 320, 320)
+    assert torch.max(sample) <= 1.0
+    assert torch.min(sample) >= -1.0
